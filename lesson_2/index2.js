2c6d557c4a82c1accaf79c1c6224922ba03757a9
Lubov Muraveva <lyubchikm@yandex.ru>
2024-03-15 23:04:40 +0300

    добаввлены файлы 2 занятия

--- lesson_2/index2.js
+++ lesson_2/index2.js
@@ -1,5 +1,21 @@
-//commit-2
-const isAdmin = true;
-isAdmin = false;
-console.log(isAdmin);
+//строковое преобразование
+const testStringVar = String(undefined);
+//преобразование к числу
+//const testNumberVar = Number("  45hjh   ");
+const testNumberVar = +undefined;
+//булевое преобразование
+//const testBooleanVar = Boolean(0);
+const testBooleanVar = !!NaN;
+
+//console.log(testBooleanVar, typeof testBooleanVar);
+
+//матем операторы
+// console.log(11%2);
+// console.log(2**3);
+
+// console.log(false+"Luba");
+
+//операторы сравнения
+// console.log(1 == true);
+console.log("a" > "A");
 
