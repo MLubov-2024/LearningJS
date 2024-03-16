ac3b57b05648c6845f790260d6074be3c73e20a8
Lubov Muraveva <lyubchikm@yandex.ru>
2024-03-16 12:15:10 +0300

    третье занятие

---
+++ lesson_3/index.js
@@ -0,0 +1,29 @@
+// const a = 1;
+// const b =2 ;
+// let result = a>b ? "a больше b"  : "a меньше b ";
+// console.log(result);
+
+// const line1 = 11;
+// const line2 = 150;
+// const line3 = 200;
+
+// if (line1 > line2) {
+//     if (line1 > line3) {
+//         console.log(line1);
+//     } 
+// }
+
+// if (line2 > line3) {
+//     if (line2 > line1) {
+//         console.log(line2);
+//     } 
+// }
+
+// if (line3 > line1) {
+//     if (line3 > line2) {
+//         console.log(line3);
+//     } 
+// }
+
+
+
